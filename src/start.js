b31f239e134d5766534702f21ef6495c462921c0
UDM2202 <ulokajidaniel@gmail.com>
2026-05-05 08:53:43 +0100

    Add start script for both services

---
+++ src/start.js
@@ -0,0 +1,13 @@
+﻿import { spawn } from 'child_process';
+
+console.log('Starting API server...');
+const server = spawn('node', ['src/server.js'], { stdio: 'inherit' });
+
+console.log('Starting Slack bot...');
+const bot = spawn('node', ['src/slack-bot.js'], { stdio: 'inherit' });
+
+process.on('SIGTERM', () => {
+  server.kill();
+  bot.kill();
+  process.exit(0);
+});
